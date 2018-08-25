27f21a02d55c9e9882433e2dc4d8e233ef2fcd4e
daiwk <daiwk@foxmail.com>
2018-08-26 01:10:02 +0800

    x

---
+++ framework/tools/demo2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: gbk -*-
+########################################################################
+# 
+# Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: demo1.py
+Author: daiwenkai(daiwenkai@baidu.com)
+Date: 2018/08/25 23:46:32
+"""
+
+class Demo2:
+    '''
+        demo
+    '''
+    def get(v):
+        '''
+        get
+        '''
+        return v
